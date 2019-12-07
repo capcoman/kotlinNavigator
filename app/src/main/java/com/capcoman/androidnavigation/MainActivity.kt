4dffcd8f0772d6c457da6a7783f08567892e62b3
Cenk <capcomin@gmail.com>
2019-12-08 00:04:05 +0400

    init app

---
+++ app/src/main/java/com/capcoman/androidnavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.capcoman.androidnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
